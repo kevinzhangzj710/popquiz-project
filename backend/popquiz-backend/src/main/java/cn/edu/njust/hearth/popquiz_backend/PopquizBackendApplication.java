fac2b864938d57eeddee2c9a08c85ddb71f1a34e
kevinzhangzj <710895909@qq.com>
2025-07-11 21:20:37 +0800

    初始化后端项目

---
+++ backend/popquiz-backend/src/main/java/cn/edu/njust/hearth/popquiz_backend/PopquizBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.njust.hearth.popquiz_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopquizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopquizBackendApplication.class, args);
+	}
+
+}
---
+++ backend/popquiz-backend/src/test/java/cn/edu/njust/hearth/popquiz_backend/PopquizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.njust.hearth.popquiz_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopquizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
